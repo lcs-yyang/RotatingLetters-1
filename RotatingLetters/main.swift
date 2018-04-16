631676f48cf1576c4929eddb0d0a9d26f70f87f8
Mesburis, Cassie <cmesburis@lcs-mba3252.lcs.on.ca>
2018-04-16 14:23:29 -0400

    g.yflyuffluyf

--- RotatingLetters/main.swift
+++ RotatingLetters/main.swift
@@ -7,11 +7,53 @@
 //
 
 import Foundation
+prompt: while 1 == 1 {
+    //INPUT
+    
+    // loop forever until we get good input
+    var validInput = "" // outside the loop -- we can use it later on AFTER
+    // Prompt the user
+    print("Input word is?")
+    
+    // Wait for input
+    // Test #1: make sure it is not nil
+    guard let givenInput = readLine() else {
+        
+        // If we got here, input is nil
+        //prompt again
+        continue  // continue with the next iteration of while Input
+    }
+    // Test #2: Is the input the correct length
+    if givenInput.count < 1 || givenInput.count > 30 {
+        
+        //if we got here, input is too short
+        continue // prompt again
+    }
+    
+    //Test #3: Check to ensure there is only UPPERCASE letters and no spaces
+    checking: for individualCharacter in givenInput {
+        
+        //DEBUG: PRint the character we are looking at
+        print(individualCharacter)
+        
+        //Check the character
+        switch individualCharacter {
+        case "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z":
+            continue checking //OK, go to next character in the string
+        default :
+            continue prompt //Stop looking, we found bad input
+        }
+    }
+    
+    //If we got to this point, we know the input is good
+    validInput = givenInput
+    break // quit the while loop and begin PROCESS section
+    
+    
+    // PROCESS
+    print(validInput)
+    
+}
+// OUTPUT
 
-// Get the user input
-var rawInput = readLine()
-
-// Print out the input provided
-print("You said:")
-print(rawInput)
 
